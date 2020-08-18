bb0b2605dca4d4a8224d320fc85f49552e0425c2
sdegaon8 <66095792+sdegaon8@users.noreply.github.com>
2020-08-18 13:37:04 -0400

    Delete ramdom1.py

--- ramdom1.py
+++
@@ -1,5 +0,0 @@
-import requests
-term = input("What would you like to search for? ")
-url = "https://icanhazdadjoke.com/search"
-response = requests.get(url, headers={"Accept" : "application/json"}, ).json()
-print(response)
