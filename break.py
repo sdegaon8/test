2267e1784dcbbad60f5db83475fd993b6c4c1cf9
sdegaon8 <66095792+sdegaon8@users.noreply.github.com>
2020-08-18 13:35:50 -0400

    Delete break.py

--- break.py
+++
@@ -1,7 +0,0 @@
-times = int(input("how many times do I have to tell you? "))
-
-for time in range(times):
-        print("Clean up the room! ")
-        if time >= 4:
-                print("do you listen anymore even?")
-                break
