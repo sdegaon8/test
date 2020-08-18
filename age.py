e0e194a9188d01c39df16f4a1c9d654d253c152c
sdegaon8 <66095792+sdegaon8@users.noreply.github.com>
2020-08-18 13:34:42 -0400

    Delete age.py

--- age.py
+++
@@ -1,8 +0,0 @@
-age = input("how old are you?")
-if age:
-    if int(age) >=18 and int(age) < 21:
-	             print("You can enter!")
-elif int(age) >= 21:
-		         print("You cant enter!")
-else:
-                 print("get lost")
